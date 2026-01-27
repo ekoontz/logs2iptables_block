31ac0e4a9e38b3398730201a740408ef322371bf
Eugene Koontz <ekoontz@hiro-tan.org>
2026-01-27 19:41:15 +0000

    new ./consolidate.sh script to help with consolidating /32 rules into /24 rules

---
+++ consolidate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Helps with consolidate individual IP blocks into /24s:
+# if there IS already a 24, then show the 'iptable -D' rules
+# which are necesssary to remove the superfluous /32 rules.
+#
+# ./top_24s.sh | awk '{print $2}' | xargs -I{} ./consolidate.sh {}
+export TWENTYFOUR_IP=$1 && ( ./get_ip_tables.sh | grep $TWENTYFOUR_IP | grep -v "/24" ) || \
+    (  ./get_ip_tables.sh | grep $TWENTYFOUR_IP | grep -v "/24" | xargs -I{} echo sudo iptables -D INPUT -s {} -p tcp --dport 25 -j DROP )
