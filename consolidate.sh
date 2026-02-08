dd34ec529a39f88201df5b3b91db4994345e1830
Eugene Koontz <ekoontz@hiro-tan.org>
2026-02-08 09:35:59 +0000

    improve usage comment

--- consolidate.sh
+++ consolidate.sh
@@ -4,6 +4,6 @@
 # if there IS already a 24, then show the 'iptable -D' rules
 # which are necesssary to remove the superfluous /32 rules.
 #
-# ./top_24s.sh | awk '{print $2}' | xargs -I{} ./consolidate.sh {}
+# ./top_24s.sh | egrep "^\s+2" | awk '{print $2}' | xargs -I{} ./consolidate.sh {}
 export TWENTYFOUR_IP=$1 && ( ./get_ip_tables.sh | grep $TWENTYFOUR_IP | grep -v "/24" ) || \
     (  ./get_ip_tables.sh | grep $TWENTYFOUR_IP | grep -v "/24" | xargs -I{} echo sudo iptables -D INPUT -s {} -p tcp --dport 25 -j DROP )
