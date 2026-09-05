2bd365a28bb52813956a779959ca3142ad089f6d
Eugene Koontz <ekoontz@hiro-tan.org>
2026-09-05 20:40:29 +0000

    improve lookup.sh script to allow multiple abuse contacts

--- lookup.sh
+++ lookup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
 
 NET=$1
-ABUSE=$(whois "$NET" | grep 'Abuse contact' | awk '{print $9}' | sed "s/'//g")
+ABUSE1=$(whois "$NET" | grep -i "Abuse Contact" | awk '{print $9}' | sed "s/'//g")
+ABUSE2=$(whois "$NET" | grep -i "OrgAbuseEmail" | awk '{print $2}' | sed "s/'//g")
 
 # Print with first column padded to 30 characters (adjust width as needed)
+ABUSE=$(echo $ABUSE1 " " $ABUSE2 | sed "s/^ //" | sed "s/ +$//")
 printf "%-30s %s\n" "$NET" "$ABUSE"
+
